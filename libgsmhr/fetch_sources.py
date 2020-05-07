7edbbb9c819c39dd17ff05e3e730e6a82716a072
Mihai Moldovan <ionic@ionic.de>
2020-05-07 10:16:46 +0200

    libgsmhr: make fetch_sources.py compatible with Python 2 and 3.

--- libgsmhr/fetch_sources.py
+++ libgsmhr/fetch_sources.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python
 
-import urllib2
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
 import os
 import sys
 import zipfile
-
-try:
-	import cStringIO as StringIO
-except:
-	import StringIO
-
+import six
 
 SRC = "http://www.3gpp.org/ftp/Specs/archive/06_series/06.06/0606-421.zip"
 
 
 def get_zipfile(data):
-	return zipfile.ZipFile(StringIO.StringIO(data))
+	return zipfile.ZipFile(six.BytesIO(data))
 
 
 def get_subfile_data(data, filename):
@@ -25,7 +23,7 @@ def get_subfile_data(data, filename):
 
 def process_file(z, e):
 	fh = open(e.filename.lower(), 'w')
-	d = z.read(e).replace('\r','')
+	d = z.read(e).decode('UTF-8').replace('\r','')
 	fh.write(d)
 	fh.close()
 
@@ -34,7 +32,7 @@ def main(*args):
 
 	# Args
 	if len(args) != 2:
-		print "Usage: %s target_dir" % args[0]
+		print("Usage: %s target_dir" % args[0])
 		return
 
 	tgt = args[1]
@@ -45,7 +43,7 @@ def main(*args):
 	os.chdir(tgt)
 
 	# Get the original data
-	u = urllib2.urlopen(SRC)
+	u = urlopen(SRC)
 	d = u.read()
 
 	# Get DISK.zip
